6bc63ced22b30b11e36b81683a498c1e5740cf8b
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-13 15:50:55 +0530

    server with html file

--- Server_HTTP/Server.js
+++ Server_HTTP/Server.js
@@ -4,6 +4,8 @@ let port = 3000;
 let server = createServer((req, res) => {//always sequeance is same as request , responce and variable name can be change {req,res} or{res,req} or {ram, abhi}
     // res.write("Hello This is server");
     // console.log(req.url);
+
+
     let log = `${new Date().toLocaleString()} , url:${req.url}`
 
     fs.writeFile('getserver.txt', log + '\n', { flag: 'a' }, (err) => {
@@ -12,20 +14,19 @@ let server = createServer((req, res) => {//always sequeance is same as request ,
     })
 
 
-    if(req.url=='/')
-    {
-        res.write("You are in Home pages");
+    if (req.url == '/') {
+        // res.writeHead(200,{'content-type':'text/plain'})
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write("<h1>You are in Home pages</h1>");
 
     }
-    else if(req.url=='/about')
-    {
+    else if (req.url == '/about') {
         res.write("You are in About page.")
     }
-    else if(req.url=='/contact')
-    {
+    else if (req.url == '/contact') {
         res.write("Welcome to contact page.")
     }
-    else{
+    else {
         res.write("404 error")
     }
 
