8a1de8c4c36881952331242d1d11661d650fd564
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-13 15:31:47 +0530

    Basic Array and HTTP Server and route

---
+++ Basic/array.js
@@ -0,0 +1,3 @@
+let data = ['ram',8,'9',true,["shayam",20,9.9],{Name:"Abhishek",CGPA:8},{Name:["Abhishek","Vivek","Nothing"], Add:function add(){}}]
+console.log(data[6].Name[1]);//Vivek
+console.log(console);
--- Server_HTTP/Server.js
+++ Server_HTTP/Server.js
@@ -1,27 +1,40 @@
-import {createServer} from 'http'
+import { createServer } from 'http'
+import fs from 'fs'
 let port = 3000;
-let server = createServer((req,res)=>{
-    res.write("Hello This is server");
+let server = createServer((req, res) => {//always sequeance is same as request , responce and variable name can be change {req,res} or{res,req} or {ram, abhi}
+    // res.write("Hello This is server");
     // console.log(req.url);
-    // if(req.url=='/')
-    // {
-    //     console.log("Everything");
-        
-    // }
-    // else if(req.closed){
-    //     console.log("Closed")
-    // }
-    // else{
-    //     console.log("Nothing")
-    // }
+    let log = `${new Date().toLocaleString()} , url:${req.url}`
+
+    fs.writeFile('getserver.txt', log + '\n', { flag: 'a' }, (err) => {
+        if (err) console.log(err);
+        else console.log('log added')
+    })
 
 
-    res.end();
+    if(req.url=='/')
+    {
+        res.write("You are in Home pages");
+
+    }
+    else if(req.url=='/about')
+    {
+        res.write("You are in About page.")
+    }
+    else if(req.url=='/contact')
+    {
+        res.write("Welcome to contact page.")
+    }
+    else{
+        res.write("404 error")
+    }
+
+    res.end();//This is mendentory
 
 });
 
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Server listing on ${port}`);
 })
 // host ke bad => http://localhost:3000/about likhana 
