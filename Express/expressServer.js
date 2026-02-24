414e3c5d1e3633fbf360a19d8a6e6563c8dd8ecc
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-02-24 11:10:12 +0530

    in ExpressServer i use the path

--- Express/expressServer.js
+++ Express/expressServer.js
@@ -1,28 +1,51 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+let __filename = fileURLToPath(import.meta.url);
+let __dirname = dirname(__filename)
+
+
+
 let server = express();
-server.get('/', (req, res) => {
-    let index = fs.readFileSync('index.html', 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(index);
-    res.end();
+server.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
 })
-server.get('/about', (req, res) => {
-    // res.send("About");
-    let about = fs.readFileSync('about.html', 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(about);
-    res.end();
+
+server.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'about.html'))
+})
+server.get('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'contact.html'))
+})
+
+// server.get('/', (req, res) => {
+//     let index = fs.readFileSync('index.html', 'utf-8');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(index);
+//     res.end();
+// })
 
 
-})
-server.get('/contact', (req, res) => {
+// server.get('/about', (req, res) => {
+//     // res.send("About");
+//     let about = fs.readFileSync('about.html', 'utf-8');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(about);
+//     res.end();
 
-    let contact = fs.readFileSync('contact.html', 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(contact);
-    res.end();
-})
+
+// })
+
+
+// server.get('/contact', (req, res) => {
+
+//     let contact = fs.readFileSync('contact.html', 'utf-8');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(contact);
+//     res.end();
+// })
 server.listen(3000, () => {
     console.log('server is ready to work')
 })
