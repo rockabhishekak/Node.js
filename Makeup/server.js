1d1631ef35b69a76912d98e511ae158c7d7eb422
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-01 17:58:46 +0530

    makeup class

--- Makeup/server.js
+++ Makeup/server.js
@@ -1,8 +1,12 @@
-import  express from 'express';
+import express from 'express';
 let server = express();
 import path from 'path';
 // import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
+import fs from 'fs';
+
+
 
 let fileName = fileURLToPath(import.meta.url);
 let dirName = path.dirname(fileName);
@@ -14,16 +18,36 @@ server.get('/', (req, res) => {
 server.get('/about', (req, res) => {
     res.sendFile(path.join(dirName, 'about.html'));
 })
-server.get('/contact', (req, res)=>{
-    res.sendFile(path.join(dirName , 'contact.html'));
+server.get('/contact', (req, res) => {
+    res.sendFile(path.join(dirName, 'contact.html'));
 })
 
 server.get('/submit', (req, res) => {
 
-    let {uname, email, password}= req.query;
-    res.send(`Username: ${uname} Email: ${email} Password: ${password}`); // for displaying the data in the browser
-    
+    // let {uname, email, password}= req.query;
+    // res.send(`Username: ${uname} Email: ${email} Password: ${password}`); // for displaying the data in the browser
+    fs.readFile('students.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Error reading file");
+        }
+        else {
+            let student = JSON.parse(data);
+            student.push(req.query);
+            fs.writeFile('students.txt', JSON.stringify(student), (err) => {
+                if (err) {
+                    console.log("Error writing file");
+                }
+                else {
+                    res.send("Data submitted successfully");
+                }
+            })
+        }
+
+
+    })
 })
+
+
 
 server.listen(3000, () => {
     console.log("server is running on port 3000");
---
+++ Makeup/serverr.js
@@ -0,0 +1,63 @@
+import express from 'express';
+let server = express();
+import path from 'path';
+// import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import fs from 'fs';
+
+
+
+let fileName = fileURLToPath(import.meta.url);
+let dirName = path.dirname(fileName);
+
+server.get('/', (req, res) => {
+    // res.send("welcome to express server");
+    res.sendFile(path.join(dirName, 'index.html'));
+})
+server.get('/about', (req, res) => {
+    res.sendFile(path.join(dirName, 'about.html'));
+})
+server.get('/contact', (req, res) => {
+    res.sendFile(path.join(dirName, 'contact.html'));
+})
+
+server.get('/submit', (req, res) => {
+
+
+
+
+    let {uname, email, password, cgpa}= req.query;
+    // res.send(`Username: ${uname} Email: ${email} Password: ${password}`); // for displaying the data in the browser
+
+    // manual validation
+    if(cgpa<0 || cgpa>=10){
+        res.send("Invalid data");
+        return;
+    }
+    fs.readFile('students.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Error reading file");
+        }
+        else {
+            let student = JSON.parse(data);
+            student.push(req.query);
+            fs.writeFile('students.txt', JSON.stringify(student), (err) => {
+                if (err) {
+                    console.log("Error writing file");
+                }
+                else {
+                    res.send("Data submitted successfully");
+                }
+            })
+        }
+
+
+    })
+})
+
+
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
