b8e6dedcd865517cba769c50d9d9f00656814098
rockabhishekak <abhishek.kumar20248@lpu.in>
2026-03-24 11:06:50 +0530

    ca_revision

--- Ca_Revision/ca1.js
+++ Ca_Revision/ca1.js
@@ -14,4 +14,27 @@ app.get("/contact",(req,res)=>{
     res.sendFile(path.join(__dirname,"contact.html"));
 });
 
-app.post("/submit")
+app.post("/submit",(req,res)=>{
+    console.log("req recieved");
+    console.log(req.body);
+    let {name, email} = req.body;
+    fs.readFile("contact.txt","utf-8",(err,data)=>{
+        let users = [];
+        if(!err && data){
+            users = JSON.parse(data);
+        };
+        users.push({name,email});
+        fs.writeFile("contact.txt",JSON.stringify(users,null,2),(err)=>{
+            if(err){
+                return res.send("Error saving contact");
+            }
+            res.send("Contact saved successfully");
+        });
+
+    });
+
+});
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+});
